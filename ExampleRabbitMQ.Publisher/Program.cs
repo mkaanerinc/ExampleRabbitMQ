e112730214e45c07a68868f510b2b2969962a57a
Mustafa Kaan Erinç <74507542+mkaanerinc@users.noreply.github.com>
2023-12-29 13:36:57 +0300

    Fanout Exchange

--- ExampleRabbitMQ.Publisher/Program.cs
+++ ExampleRabbitMQ.Publisher/Program.cs
@@ -8,15 +8,15 @@ using var connection = factory.CreateConnection();
 
 var channel = connection.CreateModel();
 
-channel.QueueDeclare("hello-queue",true,false,false);
+channel.ExchangeDeclare(exchange:"logs-fanout",durable:true,type:ExchangeType.Fanout);
 
 Enumerable.Range(1,50).ToList().ForEach(x =>
 {
-    string message = $"Message {x}";
+    string message = $"Log {x}";
 
     var messageBody = Encoding.UTF8.GetBytes(message);
 
-    channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+    channel.BasicPublish(exchange:"logs-fanout",routingKey:"", null, messageBody);
 
     Console.WriteLine($"Mesaj gönderilmiştir : {message}");
 });
--- ExampleRabbitMQ.Subscriber/Program.cs
+++ ExampleRabbitMQ.Subscriber/Program.cs
@@ -9,11 +9,17 @@ using var connection = factory.CreateConnection();
 
 var channel = connection.CreateModel();
 
+var randomQueueName = channel.QueueDeclare().QueueName; // Random name for queue
+
+channel.QueueBind(randomQueueName,"logs-fanout","",null); // Queue will be deleted when consumer stooped.
+
 channel.BasicQos(0,1,false);
 
 var consumer = new EventingBasicConsumer(channel);
 
-channel.BasicConsume("hello-queue",false,consumer);
+channel.BasicConsume(randomQueueName,false,consumer);
+
+Console.WriteLine("Loglar dinleniyor...");
 
 consumer.Received += (object sender, BasicDeliverEventArgs e) =>
 {
